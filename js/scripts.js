bb489dd1f7c27e7e1e7d65c8be1f62da2b383f2d
Student <example@example.com>
2019-09-16 18:34:57 -0700

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  var age = parseInt(prompt("How old are you?"));
+
+  if (age >= 18) {
+    $('#vote').show();
+  } else if (age === 18) {
+    $('#vote').show();
+  } else (age =< 18) {
+    $('#novote').show();
+  }
+});
